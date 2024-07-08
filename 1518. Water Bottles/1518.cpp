03260d71a0b3d34796b8a2da13b73285cc0e23f5
Gaurav <gauravrawat0259@gmail.com>
2024-07-08 09:39:30 +0530

    Updated ques no 1518

--- 1518. Water Bottles/1518.cpp
+++ 1518. Water Bottles/1518.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int numWaterBottles(int numBottles, int numExchange) {
-        int totalBottles = numBottles;
-
-        while (numBottles >= numExchange) {
-            totalBottles += numBottles / numExchange;
-            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
+        int full=numBottles;
+        int required=numExchange;
+        int count=full;
+        int empty=full;
+        int exchange;
+        int left;
+        while(empty>=required)
+        {
+            exchange=empty/required;
+            left=empty%required;
+            count+=exchange;
+            empty+=exchange+left;
         }
-
-        return totalBottles;
+        return count;
     }
 };
