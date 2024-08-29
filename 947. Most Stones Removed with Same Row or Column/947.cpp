f508aaf33ebf1c9930586c83304c49ee7666d794
Gaurav <gauravrawat0259@gmail.com>
2024-08-29 15:27:57 +0530

    Added ques no 947

---
+++ 947. Most Stones Removed with Same Row or Column/947.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int dfs(vector<vector<int>>& stones, int index, vector<bool>& visited, int n){
+        visited[index] = true;
+        int result = 0; 
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                if(((stones[i][0] == stones[index][0] or stones[i][1] == stones[index][1]))){
+                     result+=dfs(stones, i, visited, n) + 1;
+                }
+            }
+        }
+        return result; 
+    }
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int n = stones.size();
+        vector<bool> visited(n, 0);
+        int ans = 0; 
+        for(int i = 0; i < n; i++){
+            if(!visited[i]){
+                ans+=dfs(stones, i, visited, n);
+            }
+        }
+        return ans; 
+    }
+};
